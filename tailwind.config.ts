acf3ab7fb781e6209fbc3bea11a6f498bd64ec2b
Kenny <adelekekehinde06@gmail.com>
2023-07-24 13:43:10 +0100

    style: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-commissioner)"],
+      },
+      colors: {
+        primary: "hsl(181, 92%, 19%)",
+        secondary: '#535E75',
+        hover: "hsl(181, 92%, 25%)",
+        focus: "	hsl(181, 92%, 30%)",
+      },
+      backgroundImage: {
+        layoutbg: "url('/kusp-bg.png')",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
