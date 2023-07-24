ace9669e567c5a2d09d321f6d3e06cdc312e0242
Kenny <adelekekehinde06@gmail.com>
2023-07-24 13:43:46 +0100

    feat: use commissioner font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import { Commissioner } from 'next/font/google'
 
+const commissioner = Commissioner({
+  subsets: ['latin'],
+  weight: ["400", "500", "600", "700"],
+  variable: '--font-commissioner'
+})
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${commissioner.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>)
 };
 
 export default MyApp;
